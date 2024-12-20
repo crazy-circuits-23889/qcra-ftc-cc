e17155c80bbe4a0ab5060bb2c87fa5367032aac4
BlueNiteHawk835 <97478963+BlueNiteHawk835@users.noreply.github.com>
2024-12-19 20:19:27 -0500

    b

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test1.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test1.java
@@ -8,47 +8,32 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp
 public class Test1 extends OpMode {
 
-    DcMotor motorL, motorR;
+    DcMotor arm;
     //This is a variable it is stating that motorL and motorR are 2 motors
     //motor1 is the left side wheels
     //motor2 is the right side wheels
 
     @Override
     public void init() {
-        motorL = hardwareMap.get (DcMotor.class, "leftwheels");
-        motorR = hardwareMap.get (DcMotor.class, "rightwheels");
-//This is making the control hub understand what motorL and motorR are
+        arm = hardwareMap.get(DcMotor.class, "arm");
     }
 
     @Override
     public void loop() {
 
-        float xL = gamepad1.left_stick_x;
-        if (gamepad1.left_stick_x > 0) {
-            motorL.setDirection(DcMotorSimple.Direction.FORWARD);
-            motorL.setPower(xL);
+        if (gamepad2.right_trigger > 0) {
+            arm.setDirection(DcMotorSimple.Direction.FORWARD);
+            arm.setPower(1);
 
-//Test: x is horizontal so move the left stick to the right to make the motor move
-//This controls the left side motor
         }
-        float yL = gamepad1.left_stick_y;
-        if (gamepad1.left_stick_y > 0) {
-            motorL.setDirection(DcMotorSimple.Direction.REVERSE);
-            motorL.setPower(yL);
 
-            telemetry.addData("Status", "Initialized");
-            telemetry.update();
-            // Wait for the game to start (driver presses PLAY)
+        arm.setPower(.2);
 
-             // run until t
-                telemetry.addData("Status", "Running");
-                telemetry.update();
-
-
-//This should do the same thing as the code above this one but make it work for
-//the vertical movement of the joystick and it should make the motor go the opposite
-//direction of the code above
-            }
+        if (gamepad2.left_trigger > 0) {
+            arm.setDirection(DcMotorSimple.Direction.REVERSE);
+            arm.setPower(1);
 
         }
     }
+
+}
