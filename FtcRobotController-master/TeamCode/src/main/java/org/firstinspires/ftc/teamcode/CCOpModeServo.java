9163fcc74f34af242f658a20173392074e310c93
Maanya123456 <catchmemaanya@gmail.com>
2024-12-19 19:53:22 -0500

    Update CCOpModeServo.java
    removed unnecessary code

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CCOpModeServo.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CCOpModeServo.java
@@ -3,15 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-//import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 @TeleOp
 public class CCOpModeServo extends LinearOpMode {
-    public DcMotor  leftDrive   = null;
-    public DcMotor  rightDrive  = null;
     public Servo CCOpModeServo = null;
 
     @Override
@@ -22,22 +17,8 @@ public class CCOpModeServo extends LinearOpMode {
         double turn;
         double max;
 
-        // Define and Initialize Motors
-//        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-  //      rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
         CCOpModeServo  = hardwareMap.get(Servo.class, "intake");
-        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
-        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
-        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-    //    leftDrive.setDirection(DcMotor.Direction.REVERSE);
-      //  rightDrive.setDirection(DcMotor.Direction.FORWARD);
 
-        // If there are encoders connected, switch to RUN_USING_ENCODER mode for greater accuracy
-        // leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        // rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-
-        // Send telemetry message to signify robot waiting;
         telemetry.addData(">", "Robot Ready.  Press START.");    //
         telemetry.update();
 
@@ -52,22 +33,6 @@ public class CCOpModeServo extends LinearOpMode {
             // This way it's also easy to just drive straight, or just turn.
             drive = -gamepad1.left_stick_y;
             turn  =  gamepad1.right_stick_x;
-
-            // Combine drive and turn for blended motion.
-         //   left  = drive + turn;
-           // right = drive - turn;
-
-            // Normalize the values so neither exceed +/- 1.0
-            //max = Math.max(Math.abs(left), Math.abs(right));
-            //if (max > 1.0)
-            //{
-                //left /= max;
-                //right /= max;
-            //}
-
-            // Output the safe vales to the motor drives.
-            //leftDrive.setPower(left);
-            //rightDrive.setPower(right);
 
             // run until the end of the match (driver presses STOP)
             double tgtPower = 0;
