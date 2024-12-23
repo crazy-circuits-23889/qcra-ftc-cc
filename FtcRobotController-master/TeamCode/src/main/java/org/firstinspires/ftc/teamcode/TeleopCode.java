226318bb4bc7638c04190f63333b129bd88b6d7f
Dhruvaaaaaa <dkonakati@gmail.com>
2024-12-23 17:48:08 -0500

    combined code

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopCode.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopCode.java
@@ -6,11 +6,67 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
-public class TeleopCode extends LinearOpMode{
+public class TeleopCode extends LinearOpMode {
     public Servo servo = null;
-    public DcMotor leftdrive = null;
-    public DcMotor rightdrive = null;
+    public DcMotor leftDrive = null;
+    public DcMotor rightDrive = null;
+    public DcMotor forearm = null;
     private DcMotor motortest;
 
     @Override
+    public void runOpMode() {
+        double left;
+        double right;
+        double drive;
+        double turn;
+        double max;
 
+        // Define and Initialize Motors
+        leftDrive = hardwareMap.get(DcMotor.class, "leftwheels");
+        rightDrive = hardwareMap.get(DcMotor.class, "rightwheels");
+        forearm = hardwareMap.get(DcMotor.class, "forearm");
+
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        forearm.setDirection(DcMotor.Direction.FORWARD);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData(">", "Robot Ready.  Press START.");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses START)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            // Run wheels in POV mode (note: The joystick goes negative when pushed forward, so negate it)
+            // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
+            // This way it's also easy to just drive straight, or just turn.
+            drive = -gamepad1.left_stick_y;
+            turn = gamepad1.right_stick_x;
+
+            // Combine drive and turn for blended motion.
+            left = drive + turn;
+            right = drive - turn;
+
+            // Normalize the values so neither exceed +/- 1.0
+            max = Math.max(Math.abs(left), Math.abs(right));
+            if (max > 1.0) {
+                left /= max;
+                right /= max;
+            }
+
+            // Output the safe vales to the motor drives.
+            leftDrive.setPower(left);
+            rightDrive.setPower(right);
+
+            if (gamepad2.a) {
+                forearm.setPower(0.5); // Move arm up
+            } else if (gamepad2.b) {
+                forearm.setPower(-0.5); // Move arm down
+            } else if(gamepad2.x) {
+                forearm.setPower(0); // Stop arm
+            }
+        }
+    }
+}
