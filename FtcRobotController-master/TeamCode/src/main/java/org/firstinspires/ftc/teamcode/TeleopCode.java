6a944d8eee23ad0107a9ca7384cb011316dbb7c0
anish12345144 <mallepallyanish@gmail.com>
2025-01-03 18:24:38 -0500

    claw

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopCode.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopCode.java
@@ -109,7 +109,7 @@ public class TeleopCode extends LinearOpMode {
                 ClawServo.setPosition(0);
             } else if (gamepad2.y) {
                 // move to 45 degrees.
-                ClawServo.setPosition(1);
+                ClawServo.setPosition(0.25);
             } else if (gamepad2.x) {
                 // move to 180 degrees.
                 ClawServo.setPosition(1);
