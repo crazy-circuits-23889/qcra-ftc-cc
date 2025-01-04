c53f9766598138b6b4a05c2398e0d97dbd504432
Maanya123456 <catchmemaanya@gmail.com>
2025-01-04 11:07:39 -0500

    Elapsed time test, claw position set to 0.25
    Elapsed time test, claw position set to 0.25

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopCode.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopCode.java
@@ -109,10 +109,10 @@ public class TeleopCode extends LinearOpMode {
                 ClawServo.setPosition(0);
             } else if (gamepad2.y) {
                 // move to 45 degrees.
-                ClawServo.setPosition(1);
+                ClawServo.setPosition(0.25);
             } else if (gamepad2.x) {
                 // move to 180 degrees.
-                ClawServo.setPosition(1);
+                ClawServo.setPosition(0.75);
             }
             telemetry.addData("Servo Position", ClawServo.getPosition());
             telemetry.addData("Status", "Running");
