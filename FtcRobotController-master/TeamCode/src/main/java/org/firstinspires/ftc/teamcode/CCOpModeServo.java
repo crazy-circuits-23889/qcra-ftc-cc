4cf611b523fc97191e4522f5553ce12fd332e29a
Aadhya12345 <aadhya.devabhaktuni@gmail.com>
2025-01-04 11:19:00 -0500

    commit

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CCOpModeServo.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CCOpModeServo.java
@@ -6,13 +6,13 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class CCOpModeServo extends LinearOpMode {
-    public Servo servo = null;
+    public CRServo servo = null;
 
     @Override
     public void runOpMode() {
 
 
-        servo  = hardwareMap.get(Servo.class, "intake");
+        servo  = hardwareMap.get(CRServo.class, "intake");
 
         telemetry.addData(">", "Robot Ready.  Press START.");    //
         telemetry.update();
@@ -28,14 +28,14 @@ public class CCOpModeServo extends LinearOpMode {
                 //CCOpModeServo.setPower(tgtPower);
                 // check to see if we need to move the servo.
                 if(gamepad1.y) {
-                    // move to 0 degrees.
-                    servo.setPosition(0);
+
+                    servo.setPower(0);
                 } else if (gamepad1.x || gamepad1.b) {
-                    // move to 90 degrees.
-                    servo.setPosition(0.5);
+
+                    servo.setPower(-1);
                 } else if (gamepad1.a) {
-                    // move to 180 degrees.
-                    servo.setPosition(1);
+
+                    servo.setPower(1);
                 }
                 telemetry.addData("Servo Position", servo.getPosition());
                 telemetry.addData("Target Power", tgtPower);
